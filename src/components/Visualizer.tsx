2549617d13227e134a58f27d0e63f83525aa01da
Mayuran Visakan <mayuran.k.v@gmail.com>
2024-02-02 20:35:25 +0000

    Update

--- src/components/Visualizer.tsx
+++ src/components/Visualizer.tsx
@@ -8,67 +8,76 @@ import AnimatedCanvas from "./AnimatedCanvas";
 type CanvasData = {
 	themeColor: Spicetify.Color;
 	seed: number;
-	amplitudeCurve: Array<{
+	amplitudeCurve: {
 		x: number;
 		y: number;
-	}>;
+	}[];
 };
 
-type RendererState = {isError: true} | {
-	isError: false;
-	particleShader: WebGLProgram;
-	blurShader: WebGLProgram;
-	finalizeShader: WebGLProgram;
-	viewportSize: number;
+type RendererState =
+	| {
+			isError: true;
+	  }
+	| {
+			isError: false;
+			particleShader: WebGLProgram;
+			blurShader: WebGLProgram;
+			finalizeShader: WebGLProgram;
+			viewportSize: number;
 
-	inPositionLoc: number;
-	inPositionLocBlur: number;
-	inPositionLocFinalize: number;
+			inPositionLoc: number;
+			inPositionLocBlur: number;
+			inPositionLocFinalize: number;
 
-	uScaledTimeLoc: WebGLUniformLocation;
-	uAmplitudeLoc: WebGLUniformLocation;
-	uSeedLoc: WebGLUniformLocation;
-	uDotCountLoc: WebGLUniformLocation;
-	uDotRadiusLoc: WebGLUniformLocation;
-	uDotRadiusPXLoc: WebGLUniformLocation;
-	uDotSpacingLoc: WebGLUniformLocation;
-	uDotOffsetLoc: WebGLUniformLocation;
-	uSphereRadiusLoc: WebGLUniformLocation;
-	uFeatherLoc: WebGLUniformLocation;
-	uNoiseFrequencyLoc: WebGLUniformLocation;
-	uNoiseAmplitudeLoc: WebGLUniformLocation;
+			uScaledTimeLoc: WebGLUniformLocation;
+			uAmplitudeLoc: WebGLUniformLocation;
+			uSeedLoc: WebGLUniformLocation;
+			uDotCountLoc: WebGLUniformLocation;
+			uDotRadiusLoc: WebGLUniformLocation;
+			uDotRadiusPXLoc: WebGLUniformLocation;
+			uDotSpacingLoc: WebGLUniformLocation;
+			uDotOffsetLoc: WebGLUniformLocation;
+			uSphereRadiusLoc: WebGLUniformLocation;
+			uFeatherLoc: WebGLUniformLocation;
+			uNoiseFrequencyLoc: WebGLUniformLocation;
+			uNoiseAmplitudeLoc: WebGLUniformLocation;
 
-	uBlurRadiusLoc: WebGLUniformLocation;
-	uBlurDirectionLoc: WebGLUniformLocation;
-	uBlurInputTextureLoc: WebGLUniformLocation;
+			uBlurRadiusLoc: WebGLUniformLocation;
+			uBlurDirectionLoc: WebGLUniformLocation;
+			uBlurInputTextureLoc: WebGLUniformLocation;
 
-	uOutputColorLoc: WebGLUniformLocation;
-	uBlurredTextureLoc: WebGLUniformLocation;
-	uOriginalTextureLoc: WebGLUniformLocation;
+			uOutputColorLoc: WebGLUniformLocation;
+			uBlurredTextureLoc: WebGLUniformLocation;
+			uOriginalTextureLoc: WebGLUniformLocation;
 
-	quadBuffer: WebGLBuffer;
+			quadBuffer: WebGLBuffer;
 
-	particleFramebuffer: WebGLFramebuffer;
-	particleTexture: WebGLTexture;
-	blurXFramebuffer: WebGLFramebuffer;
-	blurXTexture: WebGLTexture;
-	blurYFramebuffer: WebGLFramebuffer;
-	blurYTexture: WebGLTexture;
-};
+			particleFramebuffer: WebGLFramebuffer;
+			particleTexture: WebGLTexture;
+			blurXFramebuffer: WebGLFramebuffer;
+			blurXTexture: WebGLTexture;
+			blurYFramebuffer: WebGLFramebuffer;
+			blurYTexture: WebGLTexture;
+	  };
 
-export default function Visualizer(props: {isEnabled: boolean, onError: (msg: string) => void, themeColor: Spicetify.Color, audioAnalysis?: SpotifyAudioAnalysis}) {
+export default function Visualizer(props: {
+	isEnabled: boolean;
+	onError: (msg: string) => void;
+	themeColor: Spicetify.Color;
+	audioAnalysis?: SpotifyAudioAnalysis;
+}) {
 	const amplitudeCurve = useMemo(() => {
 		if (!props.audioAnalysis) return [{ x: 0, y: 0 }];
 
 		const segments = props.audioAnalysis.segments;
 
 		const amplitudeCurve: Point2D[] = segments.flatMap(segment =>
-			segment.loudness_max_time ? [
-					{ x: segment.start, y: decibelsToAmplitude(segment.loudness_start) },
-					{ x: segment.start + segment.loudness_max_time, y: decibelsToAmplitude(segment.loudness_max) }
-			] : [
-					{ x: segment.start, y: decibelsToAmplitude(segment.loudness_start) }
-			]
+			segment.loudness_max_time
+				? [
+						{ x: segment.start, y: decibelsToAmplitude(segment.loudness_start) },
+						{ x: segment.start + segment.loudness_max_time, y: decibelsToAmplitude(segment.loudness_max) }
+				  ]
+				: [{ x: segment.start, y: decibelsToAmplitude(segment.loudness_start) }]
 		);
 
 		if (segments.length) {
@@ -79,7 +88,7 @@ export default function Visualizer(props: {isEnabled: boolean, onError: (msg: st
 			});
 		}
 
-		return  ;
+		return amplitudeCurve;
 	}, [props.audioAnalysis]);
 
 	const seed = props.audioAnalysis?.meta.timestamp ?? 0;
@@ -194,10 +203,10 @@ export default function Visualizer(props: {isEnabled: boolean, onError: (msg: st
 		gl.bindBuffer(gl.ARRAY_BUFFER, quadBuffer);
 		// prettier-ignore
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-			-1, -1,
-			-1,  1,
-			1,  1,
-			1, -1
+    		-1, -1,
+    		-1,  1,
+			 1,  1,
+    		 1, -1
 		]), gl.STATIC_DRAW);
 
 		gl.enable(gl.BLEND);
